eb9ea749b7d2cf992b1b6d556626bbae9b5f20e9
AlexeySInushkin <a.sinushkin@id-mt.ru>
2024-12-25 17:30:22 +0800

    date picker

--- python/doorbell/app.py
+++ python/doorbell/app.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from starlette.requests import Request
+from starlette.templating import Jinja2Templates
 
 from model.bell_event import BellEvent
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def read_root():
-  return "Please use /docs address "
+
+@app.get("/", response_class=HTMLResponse)
+async def dashboard(request: Request):
+    data = [
+        {"id": "1,001", "column1": "random", "column2": "data", "column3": "placeholder", "column4": "text"},
+        {"id": "1,002", "column1": "placeholder", "column2": "irrelevant", "column3": "visual", "column4": "layout"},
+        # Добавьте больше строк по необходимости
+    ]
+    return templates.TemplateResponse("dashboard.html", {"request": request, "data": data})
+
 
 @app.get("/bell_events/last_day")
 def get_bell_events():
--- python/doorbell/test/memory_store_test.py
+++ python/doorbell/test/memory_store_test.py
@@ -12,3 +12,12 @@ def test_create():
   assert 1 == len(bell_events)
   image2 = store.get_image(event.id)
   assert image[0] == image2[0]
+
+def test_limit():
+  store = InMemoryEventStore()
+  image = np.array([1, 2, 3])
+  event = store.create(image)
+  bell_events = store.get_last_events()
+  assert 1 == len(bell_events)
+  image2 = store.get_image(event.id)
+  assert image[0] == image2[0]
