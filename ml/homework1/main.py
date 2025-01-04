f3cd00b57ae66573042b08a3491d6489ec4ca6e2
AlexeySinushkin <alexeysinushkin@gmail.com>
2025-01-04 18:02:51 +0800

    StratifiedKFold

--- ml/homework1/main.py
+++ ml/homework1/main.py
@@ -78,9 +78,10 @@ def show_heat_map():
 df_dummies = df_dummies.drop(['OperatingSystems', 'Browser', 'TrafficType', 'Month_Aug', 'Month_Jul', 'VisitorType_Other'], axis=1)
 
 #Разделите набор данных на матрицу наблюдений X и вектор ответов y:
-X = df_dummies.drop('Revenue', axis=1)
-y = df_dummies['Revenue']
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, random_state=42, test_size=0.2, shuffle=True)
+X_orig = df_dummies.drop('Revenue', axis=1)
+y_orig = df_dummies['Revenue']
+#Перемешивать нужно, иначе в тест попадет больше данных за декабрь, а должно попасть пропорционально меньше
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X_orig, y_orig, random_state=42, test_size=0.2, shuffle=True)
 
 december_train = X_train[X_train['Month_Dec'] == True].count()['Month_Dec']
 december_test = X_test[X_test['Month_Dec'] == True].count()['Month_Dec']
@@ -100,36 +101,54 @@ X_test_scaled = scaler.transform(X_test)
 rf = ensemble.RandomForestClassifier(
     random_state=42
 )
-def fit_and_print_f1(model):
-    #Обучаем модель
-    model.fit(X_train, y_train)
-    #Выводим значения метрики
-    y_train_pred = model.predict(X_train)
-    print('Train: {:.2f}'.format(metrics.f1_score(y_train, y_train_pred)))
-    y_test_pred = model.predict(X_test)
-    print('Test: {:.2f}'.format(metrics.f1_score(y_test, y_test_pred)))
-fit_and_print_f1(rf)
+#Обучаем модель
+rf.fit(X_train, y_train)
+#Выводим значения метрики
+y_train_pred = rf.predict(X_train)
+print('Train: {:.2f}'.format(metrics.f1_score(y_train, y_train_pred)))
+y_test_pred = rf.predict(X_test)
+print('Test: {:.2f}'.format(metrics.f1_score(y_test, y_test_pred)))
+
+
+def random_forest_train(model, X, y):
+    # Создаём объект кросс-валидатора KFold
+    kf = model_selection.StratifiedKFold(n_splits=5)
+    # Считаем метрики на кросс-валидации k-fold
+    cv_metrics = model_selection.cross_validate(
+        estimator=model,  # модель
+        X=X,  # матрица наблюдений X
+        y=y,  # вектор ответов y
+        cv=kf,  # кросс-валидатор
+        scoring='f1',  # метрика
+        return_train_score=True  # подсчёт метрики на тренировочных фолдах
+    )
+    #print(cv_metrics)
+    print('Train k-fold mean accuracy: {:.2f}'.format(np.mean(cv_metrics['train_score'])))
+    print('Valid k-fold mean accuracy: {:.2f}'.format(np.mean(cv_metrics['test_score'])))
 
 
 rf5 = ensemble.RandomForestClassifier(
+    criterion='entropy',
     n_estimators=200,
     max_depth=5,
     min_samples_leaf=5,
     random_state=42
 )
 rf7 = ensemble.RandomForestClassifier(
+    criterion='entropy',
     n_estimators=200,
     max_depth=7,
     min_samples_leaf=5,
     random_state=42
 )
 rf12 = ensemble.RandomForestClassifier(
+    criterion='entropy',
     n_estimators=200,
     max_depth=12,
     min_samples_leaf=5,
     random_state=42
 )
 
-fit_and_print_f1(rf5)
-fit_and_print_f1(rf7)
-fit_and_print_f1(rf12)
+random_forest_train(rf5, X_train_scaled, y_train)
+random_forest_train(rf7, X_train_scaled, y_train)
+random_forest_train(rf12, X_train_scaled, y_train)
