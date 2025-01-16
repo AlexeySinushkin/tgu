ed09fb6a006ccac2de406aaf1e5b1ac744db1f2d
AlexeySinushkin <a.sinushkin@id-mt.ru>
2025-01-16 16:24:13 +0800

    ccw 90

--- cv/affine/main.py
+++ cv/affine/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+# https://en.wikipedia.org/wiki/Affine_transformation
 
 
 def warp_affine(image, M, output_shape):
@@ -19,29 +19,29 @@ def warp_affine(image, M, output_shape):
         numpy.ndarray
             Изображение после афинного преобразования.
     """
-    rows, cols, *_ = image.shape  # Определяем размеры исходного изображения
-    out_rows, out_cols, *_ = output_shape # Определяем размеры выходного изображения
+    height, width, _ = image.shape  # Определяем размеры исходного изображения
+    out_height, out_width, _ = output_shape # Определяем размеры выходного изображения
 
     # Создаем пустой массив для результата
     output = np.zeros(output_shape, dtype=image.dtype)
 
     # Итерируемся по каждому пикселю выходного изображения
-    for out_row in range(out_rows):
-        for out_col in range(out_cols):
+    for x in range(width):
+        for y in range(height):
             # Преобразуем координаты пикселя из выходного пространства в входное
-            in_col, in_row, _ = np.dot(M, [out_col, out_row, 1]).astype(int)
+            transformed_x, transformed_y,  _ = np.dot(M, [x, y,  1]).astype(int)
 
             # Проверяем, находятся ли преобразованные координаты внутри границ входного изображения
-            if 0 <= in_row < rows and 0 <= in_col < cols:
+            if 0 <= transformed_x < out_width and 0 <= transformed_y < out_height:
                 # Копируем значение пикселя из входного изображения в выходное
-                output[out_row, out_col, :] = image[in_row, in_col, :]
+                output[transformed_y, transformed_x, :] = image[y, x, :]
 
     return output
 
 def get_image():
     return plt.imread("street_2.jpeg")
 
-def rotate():
+def rotate180():
     # загрузка картинки
     image = get_image()
     # угол поворота картинки
@@ -52,11 +52,12 @@ def rotate():
 #CGAffineTransform transform = CGAffineTransformMake(cos(a),sin(a),-sin(a),cos(a),x-x*cos(a)+y*sin(a),y-x*sin(a)-y*cos(a));
     x = image.shape[1]/2
     y = image.shape[0]/2
-
+    l = x-x*np.cos(th)+y*np.sin(th)
+    m = y-x*np.sin(th)-y*np.cos(th)
     # задаем матрицу транфсормации
     M = np.float32(
-        [[np.cos(th), np.sin(th), x-x*np.cos(th)+y*np.sin(th)],
-        [-np.sin(th), np.cos(th), y-x*np.sin(th)-y*np.cos(th)],
+        [[np.cos(th), np.sin(th), l],
+        [-np.sin(th), np.cos(th), m],
         [0,0,0]],)
 
     # Размер выходного изображения
@@ -73,4 +74,30 @@ def rotate():
     #plt.tight_layout()
     plt.show()
 
-rotate()
+def rotate90CCW():
+    # загрузка картинки
+    image = get_image()
+    height, width, _ = image.shape
+    # угол поворота картинки
+    th = np.radians(90)
+    # задаем матрицу транфсормации
+    M = np.float32(
+        [[np.cos(th), np.sin(th), 0],
+        [-np.sin(th), np.cos(th), width],
+        [0,0,1]],)
+
+    # Размер выходного изображения
+    shape=(width, height, image.shape[2])
+
+    # применяем афинное преобразование
+    result = warp_affine(image, M, shape)
+    plt.subplot(121)
+    plt.title("Original")
+    plt.imshow(image)
+    plt.subplot(122)
+    plt.title("Transformed")
+    plt.imshow(result)
+    #plt.tight_layout()
+    plt.show()
+
+rotate90CCW()
